ef85f39e87526a3fc28ff9965a71ee7e03087478
Thomas Davidson <tomd1969@gmail.com>
2023-01-16 22:13:12 -0500

    Added data context to other components.

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,7 +1,9 @@
-import GalleryItem from "./GalleryItem"
+import GalleryItem from './GalleryItem'
+import { useContext } from 'react'
+import { DataContext } from '../Context/DataContext'
 
-function Gallery(props) {
-    const data = props.data.result.read()
+function Gallery() {
+    const data = useContext(DataContext)
     const display = data.map((item, i) => {
         return (
             <GalleryItem key={i} item={item} />
