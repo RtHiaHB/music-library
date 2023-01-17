05050d0af79eb4d06dbe8e46be710ebc76120811
Thomas Davidson <tomd1969@gmail.com>
2023-01-17 00:31:48 -0500

    Fixed a lot of errors...

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useState, useEffect, Fragment, Suspense } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './Components/Gallery'
 import Searchbar from './Components/Searchbar'
-import { createResource as fetchData } from './helper'
 import { DataContext } from './Context/DataContext'
 import AlbumView from './Components/AlbumView'
 import ArtistView from './Components/ArtistView'
@@ -19,15 +18,19 @@ function App() {
 
   useEffect(() => {
     if(search) {
-      console.log(search)
-      const encodedSearchTerm = encodeURIComponent(search)
-      const data = fetchData(encodedSearchTerm)
-      console.log(data)
-      // if(data.results.length > 0) {
-      //   setData(data.results)
-      // } else {
-      //   setMessage('Results not Found')
-      // }
+      const fetchData = async () => {
+        const BASE_URL = 'https://itunes.apple.com/search?term='
+        const encodedSearchTerm = encodeURIComponent(search)
+        const url = BASE_URL + encodedSearchTerm
+        const response = await fetch(url)
+        const data = await response.json()
+        if(data.results.length > 0) {
+          setData(data.results)
+        } else {
+          setMessage('Results not Found')
+        }
+      }
+      fetchData()
     }
   }, [search])
 
--- src/Components/ArtistView.js
+++ src/Components/ArtistView.js
@@ -6,6 +6,7 @@ function ArtistView() {
     const { id } = useParams()
     const [ artistData, setArtistData ] = useState([])
     const data = useContext(DataContext)
+    console.log(data)
     useEffect(() => {
         const API_URL = `http://localhost:4000/album/${id}`
         const fetchData = async () => {
@@ -25,12 +26,15 @@ function ArtistView() {
             </div>
         )
     })
-    const artistName = artistData[0].artistName
+    const bioData = artistData.filter(entry => entry.wrapperType === 'artist')
+    let artistName
+    if(bioData.length > 0) {
+        artistName = bioData[0].artistName
+    }
     return (
         <div>
             <DataContext.Provider>
                 <h2>Artist: { artistName }</h2>
-                <p>Artist Data Goes Here!</p>
                 {renderAlbums}
             </DataContext.Provider>
         </div>
